7ed4bc145ed120b1337337beb43a4f0ddc1899d6
simon <ctaoaoo@icloud.com>
2024-08-23 10:39:36 +1000

    1. add constant class

---
+++ src/main/java/org/example/common/BaseConstant.java
@@ -0,0 +1,14 @@
+package org.example.common;
+
+public class BaseConstant {
+
+    /**
+     * Email subject
+     */
+    public static final String EMAIL_SUBJECT = "DemoProject-Test-Email";
+
+    /**
+     * Email content
+     */
+    public static final String EMAIL_CONTENT = "hey %s, congratulation! you register success!";
+}
